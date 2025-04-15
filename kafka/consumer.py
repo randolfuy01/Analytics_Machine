a786988a91dec7c6c5ff05f3d6dc2054b494f763
Randolf Uy <920265148@sfsu.edu>
2025-04-14 23:30:49 -0700

    added more validation functionality

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -61,7 +61,7 @@ def load_data(message):
         cur = postgres_connection.cursor()
 
         # Validate if the game is already in db
-        if game_validation(cur, message["game_id"]):
+        if not game_validation(cur, message["game_id"]):
             logging.log(
                 f"Inserting database into the database for game {message["game_id"]}"
             )
@@ -73,9 +73,9 @@ def load_data(message):
         """
         home = " ".join(message["game_data"]["home"].split(" ")[1:])
         away = " ".join(message["game_data"]["away"].split(" ")[1:])
-        if team_validation(cur, home) == False:
+        if not team_validation(cur, home):
             insert_team(cur, home)
-        if team_validation(cur, away) == False:
+        if not team_validation(cur, away):
             insert_team(cur, away)
 
         """
@@ -83,49 +83,76 @@ def load_data(message):
         If player not in db, insert new player
         """
         for player in message["boxscore"]["home"]:
-            if player_validation(cur) == False:
+            if not player_validation(cur, home, player["pname"]):
                 insert_player(cur)
 
         for player in message["boxscore"]["away"]:
-            if player_validation(cur) == False:
+            if not player_validation(cur, home, player["pname"]):
                 insert_player(cur)
+
         """ 
         Validate game data and insert game specific data
         """
 
     except Exception as e:
         logging.error(f"Error with payload for game {message["game_id"]}")
+
     finally:
         cur.close()
 
 
-def player_validation(cur) -> bool:
-    return
+def player_validation(cur, team, player_name) -> bool:
+    try:
+        # Validate the team
+        team_validated = team_validation(team)
+        if team_validated:
+            logging.info(f"Team does not exist: {team}")
+            return False
 
-
-def insert_player(cur) -> bool:
-    return
+        # Validate the player
+        cur.execute("SELECT pname FROM player WHERE pname = %s", (player_name))
+        return cur.fetchone() is None
+    except Exception as e:
+        logging.error(f"Error with validating player {player_name}: {e}")
+        return False
 
 
 def game_validation(cur, game_id) -> bool:
-    cur.execute("SELECT id FROM game WHERE id = %s;", (game_id,))
-    return cur.fetchone() is None
+    try:
+        cur.execute("SELECT id FROM game WHERE id = %s;", (game_id,))
+        return cur.fetchone() is None
+    except Exception as e:
+        logging.error(f"Error with validating game {game_id}: {e}")
+        return False
 
 
 def team_validation(cur, mascot) -> bool:
-    cur.execute("SELECT mascot FROM team WHERE id = %s;"(mascot))
-    return cur.fetchone() is None
+    try:
+        cur.execute("SELECT mascot FROM team WHERE id = %s;"(mascot))
+        return cur.fetchone() is None
+    except Exception as e:
+        logging.error(f"Error with validating team {mascot}: {e}")
+        return False
 
 
 def insert_team(cur, team):
     pass
 
 
-def insert_game(cur, message):
+def insert_game(cur, game_id, game_date, game_location):
+    try:
+        cur.execute(
+            "INSERT INTO game VALUES (%s, %s, %s);", (game_id, game_date, game_location)
+        )
+    except Exception as e:
+        logging.error(f"Error with inserting game {game_id}: {e}")
+        return False
+
+
+def insert_player(cur):
     pass
 
 
 if __name__ == "__main__":
     logging.info("Consuming all messages")
     consume()
-    load_data()
